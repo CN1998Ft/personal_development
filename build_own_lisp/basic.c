b74d66efe6bab1afa61b76c49ee3937a62b89d34
ft_fedora <zhangfengtao1998@gmail.com>
2026-02-17 22:56:57 +0000

    Chapter 3 and bonus all done

--- build_own_lisp/basic.c
+++ build_own_lisp/basic.c
@@ -11,7 +11,7 @@ int add_together(int x, int y) {
 
 void hello_world(int n) {
     while (n > 0) {
-        puts("\nHello World!\n");
+        puts("\nHello World! from the declared functions.\n");
         n -= 1;
     }
 }
@@ -56,15 +56,49 @@ int main(){
 
     // The bonus marks:
     for (int i = 0; i < 5; i++) {
-        puts("\nHello World!\n");
+        puts("\nHello World! from for loop.\n");
     }
     int z = 0;
     while (z < 5) {
-        puts("\nHello World!\n");
+        puts("\nHello World! from while loop.\n");
         z += 1;
     }
     hello_world(5);
     // The basic types in c are:
     // int    float    double    char    bool   void
+    // THE OTHER CONDITIONAL OPERATORS
+    // There are 4 more conditional operators:
+    // <= less and equal, >= greater and equal, == equal, and != not equal.
+    // THE OTHER MATHEMATICAL/ARITHMETIC OPERATORS
+    // * multiply, / divide, % remainder (plus to print % as a char of a string
+    // use %%.
+    // The -= and += are:
+    // x -= 1 is the same as x = x - 1
+    // The do loop.
+    int x = 4;
+    do {
+        puts("\nHello Worlds! from do loop.\n");
+        x--;
+    } while ( x > 0);
+    int var = 1;
+    switch (var) {
+        case 1:
+            puts("\nThe var is 1.\n");
+            break; // break out the current loop.
+        case 2:
+            puts("\nThe var is 2.\n");
+            break;
+        default:
+            puts("\nThe var is neither 1 nor 2.\n");
+            break;
+    }
+    // The continue keyword:
+    // It skip the current iter and move to next without executing the next
+    // line of code.
+    // The typedef keyword:
+    // It is used with struct together to create struct, which is the group of
+    // different types of variables. typedef can be used to create an alias of
+    // the struct.
+    }
     return 0;
 }
