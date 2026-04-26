1f798c2a4a2d643ee52dfa6bd474f67137854355
ft_fedora <zhangfengtao1998@gmail.com>
2026-04-26 22:49:27 +0100

    chapter 7

--- build_own_lisp/parsing.c
+++ build_own_lisp/parsing.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "mpc.h"
 
 #ifdef _WIN32
@@ -11,9 +12,9 @@ static char buffer[2048];
 char* readline(char* prompt) {
     fputs(prompt, stdout);
     fgets(buffer, 2048, stdin);
-    char* cpy = malloc(strlen(buffer)+1);
+    char* cpy = malloc(strlen(buffer) + 1);
     strcpy(cpy, buffer);
-    cpy[strlen(cpy)-1] = '\0';
+    cpy[strlen(cpy) - 1] = '\0';
     return cpy;
 }
 
@@ -23,6 +24,37 @@ void add_history(char* unused) {}
 #endif
 // #include <editline/history.h>
 
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) {
+        return x + y;
+    }
+    if (strcmp(op, "-") == 0) {
+        return x - y;
+    }
+    if (strcmp(op, "*") == 0) {
+        return x * y;
+    }
+    if (strcmp(op, "/") == 0) {
+        return x / y;
+    }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    char* op = t->children[1]->contents;
+    long x = eval(t->children[2]);
+
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+    return x;
+}
 
 int main(int argc, char** argv) {
     mpc_parser_t* Number = mpc_new("number");
@@ -31,24 +63,25 @@ int main(int argc, char** argv) {
     mpc_parser_t* Lispy = mpc_new("lispy");
 
     mpca_lang(MPCA_LANG_DEFAULT,
-            "                                                       \
-            number      : /-?[0-9]+/ ;                              \
-            operator    : '+' | '-' | '*' | '/' ;                   \
-            expr        : <number> | '(' <operator> <expr>+ ')' ;   \
-            lispy       : /^/ <operator> <expr>+ /$/ ;              \
-            ",
-            Number, Operator, Expr, Lispy);
+              "                                                       \
+              number      : /-?[0-9]+/ ;                              \
+              operator    : '+' | '-' | '*' | '/' ;                   \
+              expr        : <number> | '(' <operator> <expr>+ ')' ;   \
+              lispy       : /^/ <operator> <expr>+ /$/ ;              \
+              ",
+              Number, Operator, Expr, Lispy);
 
     puts("Lispy Version 0.0.1");
     puts("Press Ctrl+c to Exit\n");
 
-    while (1){
+    while (1) {
         char* input = readline("lisp> ");
 
         add_history(input);
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
