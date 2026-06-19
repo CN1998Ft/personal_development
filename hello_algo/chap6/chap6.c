779f32ab97386b8d0388536e81ddbc6e52b5caf4
H_win_fengtao <zhangfengtao1998@gmail.com>
2026-06-19 23:27:38 +0100

    chap6 hash collision done

--- hello_algo/chap6/chap6.c
+++ hello_algo/chap6/chap6.c
@@ -6,8 +6,9 @@
 
 typedef struct {
     int set;
-    int total;
+    int len;
 } MapSet;
+
 typedef struct {
   int key;
   char *val;
@@ -124,6 +125,282 @@ void print(ArrayHashMap *hmap) {
         printf("%d -> %s\n", entries[i].key, entries[i].val);
     }
     free(set.set);
+}
+
+typedef struct Node
+{
+    Pair *pair;
+    struct Node *next;
+} Node;
+
+typedef struct {
+    int size;
+    int capacity;
+    double loadThres;
+    int extendRatio;
+    Node **buckets;
+} HashMapChaining;
+
+HashMapChaining *newHashMapChaining()
+{
+    HashMapChaining *hashMap = (HashMapChaining *)malloc(sizeof(HashMapChaining));
+    hashMap->size = 0;
+    hashMap->capacity = 4;
+    hashMap->loadThres = 2.0 / 3.0;
+    hashMap->extendRatio = 2;
+    hashMap->buckets = (Node **)malloc(hashMap->capacity * sizeof(Node *));
+    for (int i = 0; i < hashMap->capacity; i++) {
+        hashMap->buckets[i] = NULL;
+    }
+    return hashMap;
+}
+
+void delHashMapChaining(HashMapChaining *hashMap) {
+    for (int i = 0; i < hashMap->capacity; i++) {
+        Node *cur = hashMap->buckets[i];
+        while (cur) {
+            Node *tmp = cur;
+            cur = cur->next;
+            free(tmp->pair);
+            free(tmp);
+        }
+    }
+    free(hashMap->buckets);
+    free(hashMap);
+}
+
+int hashFunc(HashMapChaining *hashMap, int key) {
+    return key % hashMap->capacity;
+}
+
+double loadFactor(HashMapChaining *hashMap) {
+    return (double)hashMap->size / (double)hashMap->capacity;
+}
+
+char *get(HashMapChaining *hashMap, int key) {
+    int index = hashFunc(hashMap, key);
+    Node *cur  = hashMap->buckets[index];
+    while (cur) {
+        if (cur->pair->key == key) {
+            return cur->pair->val;
+        }
+        cur = cur->next;
+    }
+    return "";
+}
+
+void put(HashMapChaining *hashMap, int key, const char *val) {
+    if (loadFactor(hashMap) > hashMap->loadThres) {
+        extend(hashMap);
+    }
+    int index = hashFunc(hashMap, key);
+    Node *cur = hashMap->buckets[index];
+    while (cur) {
+        if (cur->pair->key == key) {
+            strcpy(cur->pair->val, val);
+            return;
+        }
+        cur = cur->next;
+    }
+    Pair *newPair = (Pair *)malloc(sizeof(Pair));
+    newPair->key = key;
+    strcpy(newPair->val, val);
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->pair = newPair;
+    newNode->next = hashMap->buckets[index];
+    hashMap->buckets[index] = newNode;
+    hashMap->size++;
+}
+
+void extend(HashMapChaining *hashMap) {
+    int oldCapacity = hashMap->capacity;
+    Node **oldBuckets = hashMap->buckets;
+    hashMap->capacity *= hashMap->extendRatio;
+    hashMap->buckets = (Node **)malloc(hashMap->capacity * sizeof(Node *));
+    for (int i = 0; i < hashMap->capacity; i++) {
+        hashMap->buckets[i] = NULL;
+    }
+    hashMap->size = 0;
+    for (int i = 0; i < oldCapacity; i++) {
+        Node *cur = oldBuckets[i];
+        while (cur) {
+            put(hashMap, cur->pair->key, cur->pair->val);
+            Node *temp = cur;
+            cur = cur->next;
+            free(temp->pair);
+            free(temp);
+        }
+    }
+    free(oldBuckets);
+}
+
+void removeItem(HashMapChaining *hashMap, int key) {
+    int index = hashFunc(hashMap, key);
+    Node *cur = hashMap->buckets[index];
+    Node *pre = NULL;
+    while (cur) {
+        if (cur->pair->key == key) {
+            if (pre) {
+                pre->next = cur->next;
+            } else {
+                hashMap->buckets[index] = cur->next;
+            }
+            free(cur->pair);
+            free(cur);
+            hashMap->size--;
+            return;
+        }
+        pre = cur;
+        cur = cur->next;
+    }
+}
+
+void print(HashMapChaining *hashMap)
+{
+    for (int i = 0; i < hashMap->capacity; i++) {
+        Node *cur = hashMap->buckets[i];
+        printf("[");
+        while (cur) {
+            printf("%d -> %s, ", cur->pair->key, cur->pair->val);
+            cur = cur->next;
+        }
+        printf("]\n");
+    }
+}
+
+typedef struct {
+    int size;
+    int capacity;
+    double loadThres;
+    int extendRatio;
+    Pair **buckets;
+    Pair *TOMBSTONE;
+} HashMapOpenAddressing;
+
+HashMapOpenAddressing *newHashMapOpenAddressing() {
+    HashMapOpenAddressing *hashMap = (HashMapOpenAddressing *)malloc(sizeof(HashMapOpenAddressing));
+    hashMap->size = 0;
+    hashMap->capacity = 4;
+    hashMap->loadThres = 2.0 / 3.0;
+    hashMap->extendRatio = 2;
+    hashMap->buckets = (Pair **)calloc(hashMap->capacity, sizeof(Pair *));
+    hashMap->TOMBSTONE = (Pair *)malloc(sizeof(Pair));
+    hashMap->TOMBSTONE->key = -1;
+    hashMap->TOMBSTONE->val = "-1";
+
+    return hashMap;
+}
+
+void delHashMapOpenAddressing(HashMapOpenAddressing *hashMap) {
+    for (int i = 0; i < hashMap->capacity; i++) {
+        Pair *pair = hashMap->buckets[i];
+        if (pair != NULL && pair != hashMap->TOMBSTONE) {
+            free(pair->val);
+            free(pair);
+        }
+    }
+    free(hashMap->buckets);
+    free(hashMap->TOMBSTONE);
+    free(hashMap);
+}
+
+int hashFunc(HashMapOpenAddressing *hashMap, int key) {
+    return key % hashMap->capacity;
+}
+
+double loadFactor(HashMapOpenAddressing *hashMap) {
+    return (double)hashMap->size / (double)hashMap->capacity;
+}
+
+int findBucket(HashMapOpenAddressing *hashMap, int key) {
+    int index = hashFunc(hashMap, key);
+    int firstTombstone = -1;
+    while (hashMap->buckets[index] != NULL) {
+        if (hashMap->buckets[index]->key == key) {
+            if (firstTombstone != -1) {
+                hashMap->buckets[firstTombstone] = hashMap->buckets[index];
+                hashMap->buckets[index] = hashMap->TOMBSTONE;
+                return firstTombstone;
+            }
+            return index;
+        }
+        if (firstTombstone == -1 && hashMap->buckets[index] == hashMap->TOMBSTONE) {
+            firstTombstone = index;
+        }
+        index = (index + 1) % hashMap->capacity;
+    }
+    return firstTombstone == -1 ? index : firstTombstone;
+}
+
+char *get(HashMapOpenAddressing *hashMap, int key) {
+    int index = findBucket(hashMap, key);
+    if (hashMap->buckets[index] != NULL && hashMap->buckets[index] != hashMap->TOMBSTONE) {
+        return hashMap->buckets[index]->val;
+    }
+    return "";
+}
+
+void put(HashMapOpenAddressing *hashMap, int key, char *val) {
+    if (loadFactor(hashMap) > hashMap->loadThres) {
+        extend(hashMap);
+    }
+    int index = findBucket(hashMap, key);
+    if (hashMap->buckets[index] != NULL && hashMap->buckets[index] != hashMap->TOMBSTONE) {
+        free(hashMap->buckets[index]->val);
+        hashMap->buckets[index]->val = (char *)malloc(sizeof(strlen(val) + 1));
+        strcpy(hashMap->buckets[index]->val, val);
+        hashMap->buckets[index]->val[strlen(val)] = '\0';
+        return;
+    }
+    Pair *pair = (Pair *)malloc(sizeof(Pair));
+    pair->key = key;
+    pair->val = (char *)malloc(sizeof(strlen(val) + 1));
+    strcpy(pair->val, val);
+    pair->val[strlen(val)] = '\0';
+
+    hashMap->buckets[index] = pair;
+    hashMap->size++;
+}
+
+void removeItem(HashMapOpenAddressing *hashMap, int key) {
+    int index = findBucket(hashMap, key);
+    if (hashMap->buckets[index] != NULL && hashMap->buckets[index] != hashMap->TOMBSTONE) {
+        Pair *pair = hashMap->buckets[index];
+        free(pair->val);
+        free(pair);
+        hashMap->buckets[index] = hashMap->TOMBSTONE;
+        hashMap->size--;
+    }
+}
+
+void extend(HashMapOpenAddressing *hashMap) {
+    Pair **bucketsTmp = hashMap->buckets;
+    int oldCapacity = hashMap->capacity;
+    hashMap->capacity *= hashMap->extendRatio;
+    hashMap->buckets = (Pair **)calloc(hashMap->capacity, sizeof(Pair *));
+    hashMap->size = 0;
+    for (int i = 0; i < oldCapacity; i++) {
+        Pair *pair = bucketsTmp[i];
+        if (pair != NULL && pair != hashMap->TOMBSTONE) {
+            put(hashMap, pair->key, pair->val);
+            free(pair->val);
+            free(pair);
+        }
+    }
+    free(bucketsTmp);
+}
+
+void print(HashMapOpenAddressing *hashMap) {
+    for (int i = 0; i < hashMap->capacity; i++) {
+        Pair *pair = hashMap->buckets[i];
+        if (pair == NULL) {
+            printf("NULL\n");
+        } else if (pair == hashMap->TOMBSTONE) {
+            printf("TOMBSTONE\n");
+        } else {
+            printf("%d -> %s\n", pair->key, pair->val);
+        }
+    }
 }
 
 int main() {
