2a7e5649c48015d8a03d8915a8f6b2daaa51d2a8
Fengtao Zhang <zhangfengtao1998@gmail.com>
2026-07-11 00:00:51 +0100

    insertion sort and quick sort

--- hello_algo/chap11/code/chap11.c
+++ hello_algo/chap11/code/chap11.c
@@ -55,6 +55,110 @@ void bubbleSortWithFlag(int nums[], int size)
     }
 }
 
+void insertionSort(int nums[], int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int base = nums[i], j = i - 1;
+        while (j >= 0 && nums[j] > base)
+        {
+            nums[j + 1] = nums[j];
+            j--;
+        }
+        nums[j + 1] = base;
+    }
+}
+
+void swap(int nums[], int i, int j)
+{
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+int partition(int nums[], int left, int right)
+{
+    int i = left, j = right;
+    while (i < j)
+    {
+        while (i < j && nums[j] >= nums[left])
+        {
+            j--;
+        }
+        while (i < j && nums[i] <= nums[left])
+        {
+            i++;
+        }
+        swap(nums, i, j);
+    }
+    swap(nums, i, left);
+    return i;
+}
+
+void quickSort(int nums[], int left, int right)
+{
+    if (left >= right)
+    {
+        return;
+    }
+    int pivot = partition(nums, left, right);
+    quickSort(nums, left, pivot - 1);
+    quickSort(nums, pivot + 1, right);
+}
+
+int medianThree(int nums[], int left, int mid, int right)
+{
+    int l = nums[left], m = nums[mid], r = nums[right];
+    if ((l <= m && m <= r) || (r <= m && m <= l))
+    {
+        return mid;
+    }
+    if ((m <= l && l <= r) || (r <= l && l <= m))
+    {
+        return left;
+    }
+    return right;
+}
+
+int partitionMedian(int nums[], int left, int right)
+{
+    int med = medianThree(nums, left, (left + right) / 2, right);
+    swap(nums, left, med);
+    int i = left, j = right;
+    while (i < j)
+    {
+        while (i < j && nums[j] >= nums[left])
+        {
+            j--;
+        }
+        while (i < j && nums[i] <= nums[right])
+        {
+            i++;
+        }
+        swap(nums, i, j);
+    }
+    swap(nums, i, left);
+    return i;
+}
+
+void quickSortTailCall(int nums[], int left, int right)
+{
+    while (left < right)
+    {
+        int pivot = partition(nums, left, right);
+        if (pivot - left < right - pivot)
+        {
+            quickSortTailCall(nums, left, pivot - 1);
+            left = pivot + 1;
+        }
+        else
+        {
+            quickSortTailCall(nums, pivot + 1, right);
+            right = pivot - 1;
+        }
+    }
+}
+
 int main()
 {
     int nums[10];
